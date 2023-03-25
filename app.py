639b8637c288b7fe3885f46c15eff1653630a828
Meina9758 <bilibilimyhx@gmail.com>
2023-03-25 19:31:15 +0900

    update

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask import send_file, make_response
 
-
 app = Flask(__name__)
 CORS(app)
 
@@ -30,10 +29,9 @@ class animaldata(db.Model):
     Profile = db.Column(db.String(255), nullable=False)
     Location = db.Column(db.JSON, nullable=False)
 
-
-class locationquery(db.Model):
-    Animal = db.Column(db.String(255), primary_key=True, nullable=False)
-    Location = db.Column(db.JSON, nullable=False)
+    # class locationquery(db.Model):
+    #     Animal = db.Column(db.String(255), primary_key=True, nullable=False)
+    #     Location = db.Column(db.JSON, nullable=False)
 
     def to_feature(self):
         return {
@@ -50,48 +48,13 @@ class locationquery(db.Model):
 
 # 前端初始化时获取所有动物的经纬度在地图上进行标记
 @app.route('/api/location', methods=['GET'])
-# def get_location():
-#     try:
-#         users = locationquery.query.all()
-#         animal = [u.Animal for u in users]
-#         location = [u.Location for u in users]
-#         jsonf = {'status':'success','data':{'Animal':animal,'Location':location}}
-#         return jsonify(jsonf)
-#     except Exception as e:
-#         return jsonify({'status': 'fail', 'error': str(e)})
-
-# def test_getlocation():
-#     # 从数据库中读取名称和经纬度转换为geojson数据返回
-#     try:
-#         # animal = animaldata.query.all()
-#         users = locationquery.query.all()
-#         animal = [u.Animal for u in users]
-#         location = [u.Location for u in users]
-#         features = {
-#             'type': 'Feature',
-#             'geometry': {
-#                 'type': 'Point',
-#                 'coordinates': location[0]
-#             },
-#             'properties': {
-#                 'animal': animal
-#             }
-#         }
-#         jsonf = {
-#             'type': 'FeatureCollection',
-#             'features': [features]
-#         }
-#         return jsonify(jsonf)
-#     except Exception as e:
-#         return jsonify({'status': 'fail', 'error': str(e)})
-
 def test_getlocation():
     try:
-        users = locationquery.query.all()
+        users = animaldata.query.all()
         features = []
-        for user in users:
-            animal = user.Animal
-            location = user.Location
+        for data in users:
+            animal = data.Animal
+            location = data.Location
             feature = {
                 'type': 'Feature',
                 'properties': {
@@ -112,13 +75,27 @@ def test_getlocation():
         return jsonify({'status': 'fail', 'error': str(e)})
 
 
+# 获取动物名对应的pdf文件名，返回给前端
+@app.route('/api/pdfname', methods=['GET'])
+def get_pdfname():
+    try:
+        name = request.args.get('name')
+        users = animaldata.query.filter(animaldata.Animal.like('%' + name + '%')).all()
+        pdfname = [u.Profile for u in users]
+        profile = ''.join(pdfname)
+        return profile
+    except Exception as e:
+        return jsonify({'status': 'fail', 'error': str(e)})
+
+
+# 根据type和动物名搜索数据库，返回给前端
 @app.route('/search', methods=['GET'])
 def search_animal_byfilter():
     name = request.args.get('name')
     search = request.args.get('type')
     try:
         # 按照类查询
-        if (search == 'all'):
+        if search == 'all':
             users = animaldata.query.filter(animaldata.Animal.like('%' + name + '%')).all()
             # users = animaldata.query.filter_by(Animal={name}).all()
             Class = [u.Class for u in users]
@@ -137,7 +114,7 @@ def search_animal_byfilter():
                          }
                 return jsonify(jsonf)
         else:
-            if (search == 'Class'):
+            if search == 'Class':
                 users = animaldata.query.filter(animaldata.Class.like('%' + name + '%')).all()
                 # users = animaldata.query.filter_by(Class={name}).all()
                 Class = [u.Class for u in users]
@@ -156,7 +133,7 @@ def search_animal_byfilter():
                              }
                     return jsonify(jsonf)
             else:
-                if (search == 'Order'):
+                if search == 'Order':
                     users = animaldata.query.filter(animaldata.Order.like('%' + name + '%')).all()
                     # users = animaldata.query.filter_by(Order={name}).all()
                     Class = [u.Class for u in users]
@@ -170,11 +147,12 @@ def search_animal_byfilter():
                         return ''
                     else:
                         jsonf = {'status': 'success',
-                                 'data': {'Class': Class,'Order': Order, 'label': Animal, 'Level': Level, 'SN': SN, 'value': Profile}
+                                 'data': {'Class': Class, 'Order': Order, 'label': Animal, 'Level': Level, 'SN': SN,
+                                          'value': Profile}
                                  }
                         return jsonify(jsonf)
                 else:
-                    if (search == 'Level'):
+                    if search == 'Level':
                         users = animaldata.query.filter(animaldata.Level.like('%' + name + '%')).all()
                         # users = animaldata.query.filter_by(Level={name}).all()
                         Class = [u.Class for u in users]
@@ -196,40 +174,15 @@ def search_animal_byfilter():
         return jsonify({'status': 'fail', 'error': str(e)})
 
 
-# @app.route('/api/queryall', methods=['GET'])
-# def api_query_first():
-#     try:
-#         # db.session.execute(text('select * from LocationQuery;'))
-#         # return jsonify({'status': 'success'})
-#         # users = LocationQuery.query.all()
-#         users = animaldata.query.all()
-#         ID = [u.ID for u in users]
-#         Class = [u.Class for u in users]
-#         Order = [u.Order for u in users]
-#         Animal = [u.Animal for u in users]
-#         Level = [u.Level for u in users]
-#         SN = [u.SN for u in users]
-#         pdf = [u.pdf for u in users]
-#         Location = [u.Location for u in users]
-#         jsonf = {'status': 'success', 'data': {'ID': ID, 'Class': Class, 'Order': Order, 'Animal': Animal,
-#                                                'Level': Level, 'SN': SN, 'pdf': pdf, 'Location': Location}}
-#         return jsonify(jsonf)
-#     except Exception as e:
-#         return jsonify({'status': 'fail', 'error': str(e)})
-
-
-# 从前端POST过来的动物名称，返回动物信息和动物简介，然后在前端覆盖页面中显示
+# 从请求pdf名称，返回动物信息和动物简介，然后在前端覆盖页面中显示
 @app.route('/pdf/<animal>', methods=['GET'])
 def search_file(animal):
     try:
-        # users = animaldata.query.filter_by(Animal=animal).all()
-        # Profile = [u.Profile for u in users]
-        # File = ''.join(Profile)
         file_path = './pdf/' + animal
         if os.path.exists(file_path):
             headers = ("Content-Disposition", f"inline;filename={file_path}")
             as_attachment = False
-            response = make_response(send_file(file_path,as_attachment=as_attachment))
+            response = make_response(send_file(file_path, as_attachment=as_attachment))
             response.headers[headers[0]] = headers[1]
             return response
         else:
@@ -239,13 +192,91 @@ def search_file(animal):
         return jsonify({'status': 'fail', 'error': str(e)})
 
 
-# 当点击地图标记点时回传经纬度，在数据库中查找该经纬度附近的动物，返回动物名称，在前端用列表或表单显示
-@app.route('/api/location/<location>', methods=['POST'])
-def search_animal_from_loc(location):
+# 根据类搜索动物的经纬度，返回给前端geojson，在地图上标记。用于filter按钮，请求格式：http://localhost:5000/api/byclass?name=${name}
+@app.route('/api/byclass', methods=['GET'])
+def search_class_from_loc():
+    name = request.args.get('name')
     try:
-        Location = animaldata.query.filter_by(Location=location).all()
-        Animal = [u.Animal for u in Location]
-        jsonf = {'status': 'success', 'data': {'Animal': Animal}}
+        users = animaldata.query_by(name).all()
+        features = []
+        for data in users:
+            animal = data.Animal
+            location = data.Location
+            feature = {
+                'type': 'Feature',
+                'properties': {
+                    'name': animal
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': location
+                }
+            }
+            features.append(feature)
+        jsonf = {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+        return jsonify(jsonf)
+    except Exception as e:
+        return jsonify({'status': 'fail', 'error': str(e)})
+
+
+# 根据目搜索动物的经纬度，返回给前端geojson，在地图上标记。用于filter按钮，请求格式：http://localhost:5000/api/byorder?name=${name}
+@app.route('/api/byorder', methods=['GET'])
+def search_order_from_loc():
+    name = request.args.get('name')
+    try:
+        users = animaldata.query_by(name).all()
+        features = []
+        for data in users:
+            animal = data.Animal
+            location = data.Location
+            feature = {
+                'type': 'Feature',
+                'properties': {
+                    'name': animal
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': location
+                }
+            }
+            features.append(feature)
+        jsonf = {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+        return jsonify(jsonf)
+    except Exception as e:
+        return jsonify({'status': 'fail', 'error': str(e)})
+
+
+# 根据濒危等级搜索动物的经纬度，返回给前端geojson，在地图上标记。用于filter按钮，请求格式：http://localhost:5000/api/bylevel?level=${level}
+@app.route('/api/bylevel', methods=['GET'])
+def search_level_from_loc():
+    level = request.args.get('level')
+    try:
+        users = animaldata.query_by(level).all()
+        features = []
+        for data in users:
+            animal = data.Animal
+            location = data.Location
+            feature = {
+                'type': 'Feature',
+                'properties': {
+                    'name': animal
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': location
+                }
+            }
+            features.append(feature)
+        jsonf = {
+            'type': 'FeatureCollection',
+            'features': features
+        }
         return jsonify(jsonf)
     except Exception as e:
         return jsonify({'status': 'fail', 'error': str(e)})
